28cbcf85e5c05063341351393bf5af3b39cc5806
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-07-11 00:25:44 +0100

    Create Sort command & parse the args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
 fn main() {
     println!("Hello, world!");
+
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Sort { path } => todo!(),
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Sort { path: PathBuf },
 }
